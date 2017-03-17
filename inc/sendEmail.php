3e9de5fafce678be017bf513a0a5840b775abfd9
FedonMan <fedonman@gmail.com>
2017-03-17 14:38:13 +0200

    updates

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,16 +1,10 @@
-﻿<?php
-
-// Replace this with your own email address
-$siteOwnersEmail = 'fedonman@open-tech.gr';
-
-
+<?php
+$siteOwnersEmail = 'fedonman@gmail.com';
 if($_POST) {
-
    $name = trim(stripslashes($_POST['contactName']));
    $email = trim(stripslashes($_POST['contactEmail']));
    $subject = trim(stripslashes($_POST['contactSubject']));
    $contact_message = trim(stripslashes($_POST['contactMessage']));
-
    // Check Name
 	if (strlen($name) < 2) {
 		$error['name'] = "Please enter your name.";
@@ -25,28 +19,21 @@ if($_POST) {
 	}
    // Subject
 	if ($subject == '') { $subject = "Message from personal website"; }
-
-
-   // Set Message
-   $message .= "Email from: " . $name . "<br />";
+	// Set Message
+	$message .= "Email from: " . $name . "<br />";
 		$message .= "Email address: " . $email . "<br />";
-   $message .= "Message: <br />";
-   $message .= $contact_message;
-   $message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
-
-   // Set From: header
-   $from =  $name . " <" . $email . ">";
-
-   // Email Headers
+	$message .= "Message: <br />";
+	$message .= $contact_message;
+	$message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
+	// Set From: header
+	$from =  $name . " <" . $email . ">";
+	// Email Headers
 	$headers = "From: " . $from . "\r\n";
 	$headers .= "Reply-To: ". $email . "\r\n";
  	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
-
-  if (!$error) {
-    $mail = mail($siteOwnersEmail, $subject, $message, $headers);
-
+  	if (!$error) {
+	    $mail = mail($siteOwnersEmail, $subject, $message, $headers);
 		if ($mail) {
 			echo "OK";
 		}
@@ -55,15 +42,10 @@ if($_POST) {
 	  }
 	} # end if - no validation error
 	else {
-
 		$response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
 		$response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
 		$response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
-		
 		echo $response;
-
 	} # end if - there was a validation error
-
 }
-
 ?>
--- js/init.js
+++ js/init.js
@@ -1,30 +1,21 @@
 /*-----------------------------------------------------------------------------------
-/*
-/* Init JS
-/*
+/* MAIN JS
 -----------------------------------------------------------------------------------*/
-
  jQuery(document).ready(function($) {
-
 /*----------------------------------------------------*/
 /* FitText Settings
 ------------------------------------------------------ */
-
   setTimeout(function() {
     $('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
   }, 100);
 
-
 /*----------------------------------------------------*/
 /* Smooth Scrolling
 ------------------------------------------------------ */
-
    $('.smoothscroll').on('click',function (e) {
 	    e.preventDefault();
-
 	    var target = this.hash,
 	    $target = $(target);
-
 	    $('html, body').stop().animate({
 	        'scrollTop': $target.offset().top
 	    }, 800, 'swing', function () {
@@ -32,92 +23,54 @@
 	    });
 	});
 
-
 /*----------------------------------------------------*/
 /* Highlight the current section in the navigation bar
 ------------------------------------------------------*/
-
 	var sections = $("section");
 	var navigation_links = $("#nav-wrap a");
-
 	sections.waypoint({
-
       handler: function(event, direction) {
-
-		   var active_section;
-
-			active_section = $(this);
-			if (direction === "up") active_section = active_section.prev();
-
-			var active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');
-
-         navigation_links.parent().removeClass("current");
-			active_link.parent().addClass("current");
-
-		},
-		offset: '35%'
-
+  		  var active_section;
+  			active_section = $(this);
+  			if (direction === "up") {
+          active_section = active_section.prev();
+        }
+  			var active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');
+        navigation_links.parent().removeClass("current");
+  			active_link.parent().addClass("current");
+  		},
+		  offset: '35%'
 	});
-
 
 /*----------------------------------------------------*/
 /*	Make sure that #header-background-image height is
 /* equal to the browser height.
 ------------------------------------------------------ */
-
    $('header').css({ 'height': $(window).height() });
    $(window).on('resize', function() {
-
         $('header').css({ 'height': $(window).height() });
         $('body').css({ 'width': $(window).width() })
    });
 
-
 /*----------------------------------------------------*/
 /*	Fade In/Out Primary Navigation
 ------------------------------------------------------*/
-
    $(window).on('scroll', function() {
-
 		var h = $('header').height();
 		var y = $(window).scrollTop();
-      var nav = $('#nav-wrap');
-
-	   if ( (y > h*.20) && (y < h) && ($(window).outerWidth() > 768 ) ) {
-	      nav.fadeOut('fast');
-	   }
-      else {
-         if (y < h*.20) {
-            nav.removeClass('opaque').fadeIn('fast');
-         }
-         else {
-            nav.addClass('opaque').fadeIn('fast');
-         }
+    var nav = $('#nav-wrap');
+    if ( (y > h*.20) && (y < h) && ($(window).outerWidth() > 768 ) ) {
+      nav.fadeOut('fast');
+    }
+    else {
+      if (y < h*.20) {
+        nav.removeClass('opaque').fadeIn('fast');
       }
-
+      else {
+        nav.addClass('opaque').fadeIn('fast');
+      }
+    }
 	});
-
-
-/*----------------------------------------------------*/
-/*	Modal Popup
-------------------------------------------------------*/
-
-    $('.item-wrap a').magnificPopup({
-
-       type:'inline',
-       fixedContentPos: false,
-       removalDelay: 200,
-       showCloseBtn: false,
-       mainClass: 'mfp-fade'
-
-    });
-
-    $(document).on('click', '.popup-modal-dismiss', function (e) {
-    		e.preventDefault();
-    		$.magnificPopup.close();
-    });
-
-
 /*----------------------------------------------------*/
 /*	Flexslider
 /*----------------------------------------------------*/
@@ -137,7 +90,6 @@
  /*----------------------------------------------------*/
  /*	typed.js
   /*----------------------------------------------------*/
-   
    $('#typed').typed({
   	strings: ["developer", "educator", "traveller", "quantum computing researcher", "deep space lover", "just a human"],
   	typeSpeed: 40,
@@ -147,11 +99,8 @@
 /*----------------------------------------------------*/
 /*	contact form
 ------------------------------------------------------*/
-
    $('form#contactForm button.submit').click(function() {
-
       $('#image-loader').fadeIn();
-
       var contactName = $('#contactForm #contactName').val();
       var contactEmail = $('#contactForm #contactEmail').val();
       var contactSubject = $('#contactForm #contactSubject').val();
@@ -159,9 +108,7 @@
 
       var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
                '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
-
       $.ajax({
-
 	      type: "POST",
 	      url: "inc/sendEmail.php",
 	      data: data,
@@ -180,11 +127,8 @@
                $('#message-warning').html(msg);
 	            $('#message-warning').fadeIn();
             }
-
 	      }
-
       });
       return false;
    });
-
 });
